88fc31935228351b95a45ebe36db75be62bb243c
LAPTOP-6IR4E4AF\Tibame_T14 <ckgckg2006@gmail.com>
2022-02-08 10:19:29 +0800

    第一次

---
+++ TicketSystem/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TicketSystem.Models;
+
+namespace TicketSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public HomeController(ILogger<HomeController> logger, RoleManager<IdentityRole> roleManager)
+        {
+            _logger = logger;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Role()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TicketSystem/Controllers/RoleApiController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleApiController : ControllerBase
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleApiController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        [HttpGet]
+        public async Task<List<IdentityRole>> CreateRoleAsync()
+        {
+            var result = await _roleManager.CreateAsync(new IdentityRole("Admin"));
+            var a = result.Succeeded;
+            var r = _roleManager.Roles.ToList();
+            return r;
+        }
+    }
+}
